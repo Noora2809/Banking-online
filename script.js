f9d7dc31ca88e72e2ba402ce6ce69fabd8cc0e1c
Noora2809 <nooraashref89@gmail.com>
2023-11-20 16:23:59 +0530

    Modified script file

--- script.js
+++ script.js
@@ -49,37 +49,54 @@ function loginUser() {
         return;
     }
 
-    if (accnum in localStorage) {
-        window.location = "./userpage.html";
-    } else {
-        alert("User is not registered....Redirecting..");
-        window.location = "./register.html";
-    }
+if (accnum in localStorage) {
+  let accountDetails = JSON.parse(localStorage.getItem(accnum));
+
+  if (paswd === accountDetails.pswd) {
+      alert("Login successful");
+      localStorage.setItem("CURRENT-USER", JSON.stringify(accountDetails))
+      window.location = "./userpage.html";
+  } else {
+      alert("Incorrect password");
+  }
+} else {
+  alert("User is not registered....Redirecting..");
+  window.location = "./register.html";
+}
 }
 let username = localStorage.getItem("USERNAME");
 console.log(username);
 
 headUser.innerHTML = `Welcome ${username}`;
 
+
 //userpage.js
 
 let total = 0;
 
-function amountDeposit(){
+function amountDeposit() {
   const depositAmount = {
-  amount: parseFloat(amt1.value),
-  pass : psd1.value
+    amount: parseFloat(amt1.value),
+    pass: psd1.value
+  }
+
+  if (depositAmount.amount <= 0 || isNaN(depositAmount.amount)) {
+    alert("Please enter a valid amount");
+    return;
+  }
+  const currentUser = JSON.parse(localStorage.getItem("CURRENT-USER"));
+
+  if (currentUser === null || currentUser.pswd !== depositAmount.pass) {
+    alert("Incorrect password");
+    return;
+  }
+
+  total += depositAmount.amount;
+  alert("Amount deposited");
+  displayBalance();
 }
 
-if(depositAmount.amount <=0 || isNaN(depositAmount.amount)){
-  alert("Please enter a valid amount")
-  return;
-}
-localStorage.setItem(depositAmount.pass, JSON.stringify(depositAmount))
-total += depositAmount.amount;
-alert("Amount deposited");
-displayBalance();
-}
+
 
 function amountWithdraw(){
   const withdrawAmount = {
@@ -91,10 +108,15 @@ function amountWithdraw(){
     return;
 }
 
+const currentUser = JSON.parse(localStorage.getItem("CURRENT-USER"));
+
+  if (currentUser === null || currentUser.pswd !== withdrawAmount.pass) {
+    alert("Incorrect password");
+    return;
+  }
 if(withdrawAmount.amount > total){
   alert("Insufficient balance");
 }else{
-  localStorage.setItem(withdrawAmount.pass , JSON.stringify(withdrawAmount))
   total -= withdrawAmount.amount
   alert("Amount withdrawn");
   displayBalance();
@@ -105,4 +127,3 @@ if(withdrawAmount.amount > total){
 function displayBalance(){
   result.innerHTML = `Account balance : ${total}`
 }
-
